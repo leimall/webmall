39cb3dd10a71f1b1d9f3ee7804bccc26df82f224
Richard Wang <wangleihd@gmail.com>
2025-04-13 21:53:18 +0800

    feat: add new usa no tax page.

---
+++ src/app/usanotax/page.tsx
@@ -0,0 +1,12 @@
+// app/search/page.tsx
+'use client';
+import { Suspense } from 'react';
+import SearchLayout from '@/components/Layout/usanotax'; // 假设已拆分组件
+
+export default function SearchPage() {
+  return (
+    <Suspense>
+      <SearchLayout />
+    </Suspense>
+  );
+}
---
+++ src/components/Layout/index/usaNoTax/index.tsx
@@ -0,0 +1,23 @@
+'use client'
+
+import ProductCardOne from '@/components/Common/Products/cardtwo'
+import { Product } from '@/types/products'
+import Title from './title'
+import React from 'react'
+import Link from 'next/link'
+
+
+export default function NewsLists({ products, title }: { products: Product[], title: string }) {
+  return (
+    <section className="mt-10">
+      <Title title={title} />
+      <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-2 xl:grid-cols-4 py-4 gap-2 sm:gap-3 md:gap-4">
+        {
+          products.map((product) => (
+            <ProductCardOne key={product.ID} product={product} />
+          ))
+        }
+      </div>
+    </section>
+  );
+}
